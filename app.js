f98c8b638044b7be7f9bd0c885845ad616cf9f29
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-04-17 17:44:47 +0200

    npm config and yargs setup

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+
 const getNotes = require('./notes')
 
-const message = getNotes()
+// add command
+const addHandler = () => {
+    console.log('Adding new note...')
+}
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note to user list',
+    handler: addHandler
+})
 
-console.log(message)
+// remove command
+const removeHandler = () => {
+    console.log('Removing note...')
+}
+yargs.command({
+    command: 'remove',
+    describe: 'Remove specific note',
+    handler: removeHandler
+})
+
+// read command
+const readHandler = () => {
+    console.log('Note 123')
+}
+yargs.command({
+    command: 'read',
+    describe: 'Reads specific note',
+    handler: readHandler
+})
+
+// list command
+const listHandler = () => {
+    console.log('All notes')
+}
+yargs.command({
+    command: 'list',
+    describe: 'Lists all notes',
+    handler: listHandler
+})
+
+console.log(yargs.argv)
